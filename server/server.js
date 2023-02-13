900d7e1f660c3d0dbe9f7c84cce0ee00fb65ccc9
Hhb <33392861+764777472@users.noreply.github.com>
2023-02-13 15:05:15 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,11 +28,11 @@ app.post('/', async(req, res) => {
         const response = await openai.createCompletion({
   model:"text-davinci-003",
   prompt: `${prompt}`,
-  temperature:0.1,
+  temperature:0.9,
   max_tokens:3000,
   top_p:1,
-  frequency_penalty:0.5,
-  presence_penalty:0,
+  frequency_penalty:0.1,
+  presence_penalty:0.1,
         });
 
         res.status(200).send({
