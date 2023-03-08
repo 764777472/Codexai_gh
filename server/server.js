7b1e830dca602097056e01e12bd31f9d4ffbc558
Hhb <33392861+764777472@users.noreply.github.com>
2023-03-08 18:08:50 +0800

    Update server.js

--- server/server.js
+++ server/server.js
@@ -28,9 +28,9 @@ app.post('/', async(req, res) => {
           model: "text-davinci-003",
           prompt: `${prompt}`,
           temperature: 0.9,
-          max_tokens: 150,
+          max_tokens: 3000,
           top_p: 1,
-          frequency_penalty: 0,
+          frequency_penalty: 0.4,
           presence_penalty: 0.6
         });
         res.status(200).send({
